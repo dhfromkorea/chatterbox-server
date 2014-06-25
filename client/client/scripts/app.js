5da8389b6600a6998d5c3deaca0a6f956512cbea
dhfromkorea <dhllee424@gmail.com>
2014-06-24 21:44:24 -0700

    set up express and completed the following - data persistence : messages are logged to the local - POST, GET request handled using express

--- client/client/scripts/app.js
+++ client/client/scripts/app.js
@@ -1,107 +1,92 @@
 // YOUR CODE HERE:
 var app = {};
 
+// =MODEL
 var Message = Backbone.Model.extend({
-  idAttribute: 'objectId'
+  idAttribute: 'objectId',
+  defaults: {
+    username: 'John Doe',
+    text: 'No message',
+    roomname: 'Default lobby'
+  }
 });
 
+// =COLLECTION
 var Room = Backbone.Collection.extend({
-  // url: 'http://localhost:3000/classes/messages',
-  url: 'http://chatter-server.herokuapp.com/classes/messages',
+  url: 'http://localhost:3000/classes/messages',
   model: Message,
   parse: function(response) {
     return response.results;
   },
   initialize: function() {
-    setInterval(this.fetch.bind(this, {
-      data: {
-        'order': '-createdAt'
-      }
-    }), 1000);
+    // this.fetch.bind(this);
+    setInterval(this.fetch.bind(this), 1000);
   }
 });
 
+
+// =VIEW
 var RoomView = Backbone.View.extend({
-  initialize: function() {
-    this.collection.on('add', function(model) {
-      var messageInstance = new MessageView({
-        model: model
-      });
-      this.$el.prepend(messageInstance.render().el);
-    }, this);
-    // var roomname = messageInstance.attributes.roomname;
-    // if (!_.contains(this.collection.roomnames, roomname)) {
-    //   this.collection.roomnames.push(roomname);
-    //   this.collection.addRoom(roomname);
-    // }
-  }
-});
-
-var MessageView = Backbone.View.extend({
-  tagName: 'li',
-  render: function() {
-    var message = this.model.attributes;
-    var template = _.template('<span class="username"><%= _.escape(username) %> : </span><span class="message" data-id="<%= _.escape(objectId) %>"><%= _.escape(message) %> </span>');
-    message.message = message.message || '';
-    message.username = message.username || '';
-    message.objectId = message.objectId || '';
-    this.$el.html(template(message)).addClass('panel');
-    return this;
-  }
-});
-
-var SubmitView = Backbone.View.extend({
-  model: Message,
   events: {
-    'submit': 'submit'
+    'click .submit': 'submit'
+  },
+  initialize: function() {
+    _.bindAll(this, 'render', 'submit');
+    this.render();
+    this.collection.on('add', function(model) {
+      this.render();
+    }, this);
+  },
+  render: function() {
+    this.$('#chats').children().detach();
+    this.$('#chats').append(this.collection.map(function(message) {
+      var messageView = new MessageView({
+        model: message
+      });
+      return messageView.render();
+    }).reverse());
   },
   submit: function(e) {
     e.preventDefault();
     var message = {
       text: this.$('input[name=message]').val(),
-      username: window.location.search.split('?username=')[1]
-      // roomname prop should be created here.
+      username: window.location.search.split('?username=')[1],
+      roomname: 'default'
     };
     if (message.text) {
       this.collection.create({
         text: message.text,
-        username: message.username
+        username: message.username,
+        roomname: message.roomname
       });
       $("input[name=message]").val("");
     }
   }
 });
 
-var SelectView = Backbone.View.extend({
-  events: {
-    'change': 'chooseRoom'
-  },
-  chooseRoom: function(e) {
-    e.preventDefault();
-    var roomname = this.$('label').val();
-    this.collection.fetch({
-      'order': '-createdAt',
-      'where': {
-        'roomname': roomname
-      }
-    });
+var MessageView = Backbone.View.extend({
+  tagName: 'li',
+  template: _.template('<span class="username"><%= _.escape(username) %> : </span><span class="message"><%= _.escape(text) %> </span>'),
+  render: function() {
+    return this.$el.html(this.template(this.model.attributes)).addClass('panel');
   }
 });
-// keep a list of friends
-// keep a list of rooms that appeared
+
+// var SelectView = Backbone.View.extend({
+//   events: {
+//     'change': 'chooseRoom'
+//   },
+//   chooseRoom: function(e) {
+//     e.preventDefault();
+//     var roomname = this.$('label').val();
+//     this.collection.fetch();
+//   }
+// });
 
 $(function() {
   var room = new Room();
   var roomView = new RoomView({
     collection: room,
-    el: $('#chats')
-  });
-  var submitView = new SubmitView({
-    collection: room,
-    el: $('#send')
-  });
-  var selectView = new SelectView({
-    collection: room,
-    el: $('#rooms')
+    el: $('.app')
   });
 });
--- server/basic-server.js
+++ server/basic-server.js
@@ -1,48 +1,95 @@
 /* Import node's http module: */
+
+
+// express 
+
 var express = require('express');
-var app = express();
-
-
-
-var http = require("http");
 var fs = require('fs');
-var url = require('url');
-// var handleRequest = module.exports.handleRequest
-// var handleRequest = require("./request-handler");
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(express.static(__dirname + '/../client/client'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.saveMessages = function(data) {
+  data = JSON.stringify(data) + ';';
+  fs.appendFile('./server/messages.txt', data, function(err) {
+    if (err) {
+      throw err;
+    } else {
+      console.log('saved successfully');
+    }
+  });
+};
+
+app.readMessages = function() {
+  var data = {};
+  var jsonString = fs.readFileSync('./server/messages.txt');
+  data.results = String.prototype.split.call(jsonString, ';');
+  // cut off the empty item in the array, created by a trailing comma
+  data.results.splice(data.results.length - 1);
+  data.results = data.results.map(function(item) {
+    return JSON.parse(item);
+  });
+  return data;
+};
+
+app.get('/classes/messages', function(req, res) {
+  res.send(200, this.readMessages());
+});
+
+app.get('*', function(req, res) {
+  res.send(404);
+});
+
+app.post('/classes/messages', function(req, res) {
+  this.saveMessages(req.body);
+  res.send(201, 'poseted successfully');
+});
+
+// enable to run locally
+// app.listen(process.env.PORT || 3000);
 
 
 // heroku
 
-// var port = Number(process.env.PORT || 5000);
-// var server = http.createServer(handleRequest.handler);
-// server.listen(port);
+var port = Number(process.env.PORT || 5000);
+app.listen(port);
+
 
 // local test
-
-var port = 8000;
-var server = http.createServer(function(req, res) {
-  var urlParts = url.parse(req.url).pathname;
-  if (urlParts === '/') {
-    urlParts = '/index.html';
-  }
-  var filePath = './client/client' + urlParts;
-  console.log(filePath);
-  fs.readFile(filePath, function(err, data) {
-    if (!err) {
-      res.writeHead(200, {
-        'Content-Type': 'text/html'
-      });
-      res.end(data);
-    } else {
-      res.writeHead(500, {
-        'Content-Type': 'text/plain'
-      });
-      res.end('errrorooooorrr');
-    }
-  });
-
-});
-// var server = http.createServer(handleRequest.handler);
-var ip = '127.0.0.1';
-server.listen(port, ip);
-console.log("Listening on http://" + ip + ":" + port);
+  
+  // var server = http.createServer(handleRequest.handler);
+  // var http = require("http");
+  // var url = require('url');
+  // var handleRequest = module.exports.handleRequest
+  // var handleRequest = require("./request-handler");
+  // var port = 8000;
+  // var server = http.createServer(function(req, res) {
+  //   var urlParts = url.parse(req.url).pathname;
+  //   if (urlParts === '/') {
+  //     urlParts = '/index.html';
+  //   }
+  //   var filePath = './client/client' + urlParts;
+  //   console.log(filePath);
+  //   fs.readFile(filePath, function(err, data) {
+  //     if (!err) {
+  //       res.writeHead(200, {
+  //         'Content-Type': 'text/html'
+  //       });
+  //       res.end(data);
+  //     } else {
+  //       res.writeHead(500, {
+  //         'Content-Type': 'text/plain'
+  //       });
+  //       res.end('errrorooooorrr');
+  //     }
+  //   });
+  // });
+  // var server = http.createServer(handleRequest.handler);
+  // var ip = '127.0.0.1';
+  // server.listen(port, ip);
+  // console.log("Listening on http://" + ip + ":" + port);
